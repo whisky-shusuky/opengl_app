aeed3fe1a2b316cdbd95fdb8313ce1f2e6076a87
Whisky  shusuky <whisky.shusuky@gmail.com>
2022-12-12 18:59:20 +0900

    fix(comment):処理の解説を記載

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
+    // unwrap はエラー処理をサボっているので、実際にはエラー処理を書く
     let window = video_subsystem
         .window("SDL", 640, 480)
         .position_centered()
@@ -14,25 +15,35 @@ fn main() {
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    // 色指定
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
     canvas.clear();
     canvas.present();
 
+    // イベントループ
+    // EventPump はイベントを取得するための構造体
+    //
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
+        // event_pump.poll_iter()で得られたイテレータをループで回してイベントキューをひとつづつ処理する
         for event in event_pump.poll_iter() {
+            // rustはswitchではなくてmatch式で分岐させる
             match event {
+                // ウィンドウの×ボタンが押されたらループを抜ける
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
+                // break 'running でrunningラベルのループを抜ける
                 } => break 'running,
                 _ => {}
             }
         }
     }
 
+    // ループの中で何度も画面を白く塗りつぶす
     canvas.present();
 
-    std::thread::sleep(Duration::new(0, 1_000_000 / 60));
+    // 1/60秒待つ(60FPS)
+    std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
 }
